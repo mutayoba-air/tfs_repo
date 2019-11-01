08e490ba54f27bf5fbcfcfdadcf0254d2d5cf2f4
mutayobair <mutayoba.air@gmail.com>
2019-11-01 16:44:05 -0400

    Adding sgs, nacls, rts, subnets and nacl rules

---
+++ sgs/sgs.tf
@@ -0,0 +1,118 @@
+provider "aws" {
+  region  = "${var.region}"
+  profile = "${var.profile}"
+}
+
+variable "region" {
+  description = "Target region"
+  default     = "us-east-1"
+}
+
+variable "profile" {
+  description = "The profile you want to use"
+  default     = "default"
+}
+
+resource "aws_vpc" "test_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "test_vpc"
+  }
+}
+
+resource "aws_security_group" "allow_tlsA" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.test_vpc.id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "10.0.1.0/24" # add a CIDR block here
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.1.0/24"]
+    #prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
+resource "aws_security_group" "allow_tlsB" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.test_vpc.id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "10.0.2.0/24" # add a CIDR block here
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.2.0/24"]
+    #prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
+resource "aws_security_group" "allow_tlsC" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.test_vpc.id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "10.0.3.0/24" # add a CIDR block here
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.3.0/24"]
+    #prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
+resource "aws_security_group" "allow_tlsD" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.test_vpc.id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = "10.0.4.0/24" # add a CIDR block here
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.4.0/24"]
+    #prefix_list_ids = ["pl-12c4e678"]
+  }
+}
